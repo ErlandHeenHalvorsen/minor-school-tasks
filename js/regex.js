58a26fdbb9063b3f9f8bb8ad376ffaa13dd1ceae
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-08-29 12:36:10 +0200

    task 1,2 basic regexp

---
+++ js/regex.js
@@ -0,0 +1,14 @@
+const str = `In 2023, the world population is estimated to be around 7.9 billion. 
+This number includes people from all walks of life, from different cultures, and with diverse backgrounds.
+
+`;
+
+const regex = /\s/gm;
+
+let newStr = str.replace(regex, "");
+console.log(newStr);
+
+const mobile = `+4745403110`;
+const regex2 = /(\+)(47)(4|9)(\d{7})/gm;
+let match = mobile.match(regex2);
+console.log(match);
