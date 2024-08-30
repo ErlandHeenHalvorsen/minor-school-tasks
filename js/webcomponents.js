c67a0daf3de844208e297643024990acd666116f
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-08-30 13:43:14 +0200

    working with components

---
+++ js/webcomponents.js
@@ -0,0 +1,34 @@
+class HelloWorld extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "closed" });
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+    const greeting = document.createElement("p");
+    greeting.textContent = "Hello World";
+    container.appendChild(greeting);
+    shadow.appendChild(container);
+  }
+}
+//customElements.define("hello-world", HelloWorld);
+
+class FilledCircle extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.setAttribute("cx", "50");
+    circle.setAttribute("cy", "50");
+    circle.setAttribute("r", "40");
+    circle.setAttribute("stroke", "black");
+    circle.setAttribute("stroke-width", "4");
+    circle.setAttribute("fill", this.getAttribute("color"));
+    svg.appendChild(circle);
+    shadow.appendChild(svg);
+  }
+}
+customElements.define("filled-circle", FilledCircle);
