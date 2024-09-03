0e7840a768a28a648707ec5b073706ee8db658fb
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-09-03 13:05:56 +0200

    finished library

--- js/book_library.js
+++ js/book_library.js
@@ -46,3 +46,97 @@ class eBook extends book {
     return `${super.getInfo()}, fileSize: ${this.#fileSize} MB`;
   }
 }
+
+class printedBook extends book {
+  #numPages;
+  constructor(title, author, isbn, numPages) {
+    super(title, author, isbn);
+    this.#numPages = numPages;
+  }
+  getInfo() {
+    return `${super.getInfo()}, Pages: ${this.#numPages} `;
+  }
+}
+
+class LibraryMember {
+  #name;
+  #memberId;
+  #borrowedBooks;
+  constructor(name, memberId) {
+    this.#name = name;
+    this.#memberId = memberId;
+    this.borrowedBook = [];
+  }
+  borrowedBooks(book) {
+    if (book.isAvailable()) {
+      book.borrowBook();
+      this.#borrowedBooks.push(book);
+    } else {
+      console.log(`${book.getInfo()} is not available`);
+    }
+  }
+
+  returnBook(book) {
+    const index = this.#borrowedBooks.indexOf(book);
+    if (index !== -1) {
+      this.#borrowedBooks.splice(index, 1);
+      book.returnBook();
+    } else {
+      console.log(`${book.getInfo()} is not borrowed`);
+    }
+  }
+
+  getBorrowedBooks() {
+    return this.#borrowedBooks.map((book) => book.getTitle());
+  }
+  getMemberId() {
+    return this.#memberId;
+  }
+}
+
+class Library {
+  #books;
+  #members;
+  constructor() {
+    this.#books = [];
+    this.#members = [];
+  }
+  addBook(book) {
+    this.#books.push(book);
+  }
+  addMember(member) {
+    this.#members.push(member);
+  }
+  findBookByTitle(title) {
+    return this.#books.find((book) => book.getTitle() === title);
+  }
+  findMemberById(id) {
+    return this.#members.find((member) => member.getMemberId() === id);
+  }
+
+  borrowBook(title, memberId) {
+    const member = this.findMemberById(memberId);
+    const book = this.findBookByTitle(title);
+    if (book && member) {
+      member.borrowBook(book);
+    } else {
+      console.log("Book or member not found");
+    }
+  }
+
+  returnBook(title, memberId) {
+    const member = this.findMemberById(member);
+    const book = this.findBookByTitle(title);
+    if (book && member) {
+      member.returnBook(book);
+    } else {
+      console.log("Book or member not found");
+    }
+  }
+
+  getAvailableBooks() {
+    return this.#books
+      .filter((book) => book.isAvailable())
+      .map((book) => book.getTitle());
+  }
+}
