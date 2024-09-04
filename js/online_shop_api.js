0daf8afadcdfe08aee3cd06e293517b5fce42fbd
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2024-09-04 17:05:20 +0200

    struggeling to authorize

--- js/online_shop_api.js
+++ js/online_shop_api.js
@@ -1,10 +1,27 @@
 const baseUrl = "https://v2.api.noroff.dev";
 
-async function getOnlineShop() {
-  let response = await fetch(`${baseUrl}/online-shop`);
-  const res = await response.json();
-  const data = res.data;
-  console.log(data);
+async function authorize() {
+  try {
+    const response = await fetch(`${baseUrl}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "erlhal01002@stud.noroff.no",
+        password: "fisketur123",
+      }),
+    });
+
+    if (response.ok) {
+      console.log("Registration successful");
+    } else {
+      const errorData = await response.json(); // Parse the error response
+      console.error("Registration failed:", errorData);
+    }
+  } catch (error) {
+    console.error("Network error:", error);
+  }
 }
 
-getOnlineShop();
+authorize();
